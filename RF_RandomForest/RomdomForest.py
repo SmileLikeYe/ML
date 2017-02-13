d6e56aaa6d2c18f75080d306cdf44df96b23377c
Hu, Smile <smile.hu@sap.com>
2017-02-13 23:43:41 +0800

    Add RandomForest

---
+++ RF_RandomForest/RomdomForest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf8
+
+import sys
+reload(sys)
+sys.setdefualtencoding('utf8')
+
+'''
+Competition URL: https://www.kaggle.com/c/digit-recognizer
+Solution:  Random Forest
+'''
+
